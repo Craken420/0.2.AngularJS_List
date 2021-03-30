bae9b8e50676d77ac5fc5bc74a4682508dd1c1ef
Luis Angel Peña <lapena@mavi.mx>
2021-03-30 17:22:03 -0600

    feat:: Add the create new task function

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-
 // Modules: encapsulate to reuse
 angular.module('app', ['ngRoute'])
 .value('Tasks', [
-    {_id: 1, title: 'Master HTML/CSS/Javascript', completed: true},
-    {_id: 2, title: 'Learn AngularJS', completed: true },
-    {_id: 3, title: 'Get started with ExpressJS', completed: false },
-    {_id: 4, title: 'Setup MongoDB database', completed: false},
-    {_id: 5, title: 'Be awesome!', completed: false}
+    {_id: 1, title: 'Master HTML/CSS/Javascript', nota: 0, completed: true},
+    {_id: 2, title: 'Learn AngularJS', nota: 0, completed: true },
+    {_id: 3, title: 'Get started with ExpressJS', nota: 0, completed: false },
+    {_id: 4, title: 'Setup MongoDB database', nota: 0, completed: false},
+    {_id: 5, title: 'Be awesome!', nota: 0, completed: false}
 ])
 //---------------
 // Services
@@ -20,6 +19,11 @@ angular.module('app', ['ngRoute'])
         },
         getListItems: function() {
             return _items;
+        },
+        create: function (item) {
+            item.id = _items.length;
+            _items.push(item);
+            return _items;
         }
     }
 })
@@ -27,15 +31,29 @@ angular.module('app', ['ngRoute'])
 // Controllers
 //---------------
 .controller('TodoController', function ($scope, TaskService) {
-    $scope.students = [];
-    var empid = 1;
-    $scope.todos = TaskService.getListItems();
+    $scope.tasks = [];
+
+    $scope.create = function(newTask) {
+        $scope.tasks = TaskService.create(newTask);
+    }
+    $scope.save = function () {
+        if ($scope.newTask._id == null) {
+            $scope.create($scope.newTask)
+        }
+        $scope.newTask = {};
+    }
+
+    $scope.init = function() {
+        $scope.todos = TaskService.getListItems();
+        console.log('$scope.todos : ', $scope.todos )
+    }
+    $scope.init();
 })
-.controller('TodoDetailsCtrl', ['$scope', '$routeParams', 'TaskService',
+.controller('TodoDetailsCtrl',
     function ($scope, $routeParams, TaskService) {
         $scope.todo = TaskService.getListItems()[$routeParams.id];
     }
-])
+)
 //---------------
 // Routes
 //---------------
